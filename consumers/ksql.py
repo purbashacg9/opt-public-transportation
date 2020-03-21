9871c48052a40253f67b9f0b6280ef494365d3f5
purbasha <purbasha@paradata.io>
2020-03-20 22:36:19 -0700

    added queries for creating tables for turnstile and turnstile_summary

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -12,25 +12,33 @@ logger = logging.getLogger(__name__)
 
 KSQL_URL = "http://localhost:8088"
 
-#
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
-#       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
-#       `turnstile` table and grouping on station_id.
-#       Make sure to cast the COUNT of station id to `count`
-#       Make sure to set the value format to JSON
+
+# For the first statement, create a `turnstile` table from your turnstile topic.
+# Make sure to use 'avro' datatype!
+# For the second statment, create a `turnstile_summary` table by selecting from the
+# `turnstile` table and grouping on station_id.
+# Make sure to cast the COUNT of station id to `count`
+# Make sure to set the value format to JSON
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id VARCHAR, 
+    station_name VARCHAR, 
+    line VARCHAR, 
+    num_entries INT
 ) WITH (
-    ???
+    KAFKA_TOPIC='org.chicago.transit.looptrain.logan_square.turnstile', 
+    VALUE_FORMAT='Avro', 
+    KEY='station_id'
 );
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (
+    VALUE_FORMAT = 'JSON'
+) AS
+    SELECT station_id, CAST(COUNT(station_id) as INT)
+    FROM turnstile 
+    GROUP BY station_id;
 """
 
 
