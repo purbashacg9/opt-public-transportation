a4aa0949879cb8ff40f01b314a3234680e83985b
purbasha <purbasha@paradata.io>
2020-03-22 02:37:09 -0700

    Add consume, on_Assign and consumer initialization

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -2,7 +2,7 @@
 import logging
 
 import confluent_kafka
-from confluent_kafka import Consumer
+from confluent_kafka import Consumer, OFFSET_BEGINNING
 from confluent_kafka.avro import AvroConsumer
 from confluent_kafka.avro.serializer import SerializerError
 from tornado import gen
@@ -10,6 +10,8 @@ from tornado import gen
 
 logger = logging.getLogger(__name__)
 
+BROKER_LIST  =  "PLAINTEXT://localhost:9092, PLAINTEXT://localhost:9093, PLAINTEXT://localhost:9094"
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
 
 class KafkaConsumer:
     """Defines the base kafka consumer class"""
@@ -29,48 +31,39 @@ class KafkaConsumer:
         self.sleep_secs = sleep_secs
         self.consume_timeout = consume_timeout
         self.offset_earliest = offset_earliest
-
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
+        
+        # Configure the broker properties below. Make sure to reference the project README
         # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
-        self.broker_properties = {
-                #
-                # TODO
-                #
+        self.broker_properties = { 
+            "bootstrap.servers": BROKER_LIST, 
+            "group.id": f"consumer_group_{self.topic_name_pattern}",
+            "auto.offset.reset": "earliest"
         }
 
-        # TODO: Create the Consumer, using the appropriate type.
+        # Create the Consumer, using the appropriate type.
         if is_avro is True:
-            self.broker_properties["schema.registry.url"] = "http://localhost:8081"
-            #self.consumer = AvroConsumer(...)
+            self.broker_properties["schema.registry.url"] = SCHEMA_REGISTRY_URL
+            #TODO what to about the schema readers? 
+            self.consumer = AvroConsumer(
+                config = self.broker_properties
+            )
         else:
-            #self.consumer = Consumer(...)
-            pass
+            self.consumer = Consumer(
+                config = self.broker_properties
+            )
 
-        #
-        #
-        # TODO: Configure the AvroConsumer and subscribe to the topics. Make sure to think about
+        # Configure the AvroConsumer and subscribe to the topics. Make sure to think about
         # how the `on_assign` callback should be invoked.
-        #
-        #
-        # self.consumer.subscribe( TODO )
+        self.consumer.subscribe([self.topic_name_pattern], on_assign = self.on_assign)
 
     def on_assign(self, consumer, partitions):
         """Callback for when topic assignment takes place"""
-        # TODO: If the topic is configured to use `offset_earliest` set the partition offset to
+        # If the topic is configured to use `offset_earliest` set the partition offset to
         # the beginning or earliest
-        logger.info("on_assign is incomplete - skipping")
-        for partition in partitions:
-            pass
-            #
-            #
-            # TODO
-            #
-            #
-
+        if self.offset_earliest:
+            for partition in partitions:
+                partition.offset = OFFSET_BEGINNING
+        
         logger.info("partitions assigned for %s", self.topic_name_pattern)
         consumer.assign(partitions)
 
@@ -84,15 +77,19 @@ class KafkaConsumer:
 
     def _consume(self):
         """Polls for a message. Returns 1 if a message was received, 0 otherwise"""
-        #
-        #
-        # TODO: Poll Kafka for messages. Make sure to handle any errors or exceptions.
+        # Poll Kafka for messages. Make sure to handle any errors or exceptions.
         # Additionally, make sure you return 1 when a message is processed, and 0 when no message
         # is retrieved.
-        #
-        #
-        logger.info("_consume is incomplete - skipping")
-        return 0
+        message = self.consumer.poll(1.0)
+        if message is None:
+            logger.info("no message received by consumer")
+            return 0
+        elif message.error() is not None:
+            logger.info(f"error from consumer {message.error()}")
+        else:
+            logger.info(f"consumed message {message.key()}: {message.value()}")
+            self.message_handler(message)
+        return 1
 
 
     def close(self):
@@ -100,5 +97,5 @@ class KafkaConsumer:
         #
         #
         # TODO: Cleanup the kafka consumer
-        #
-        #
+        # finish consuming any messages that the consumer has polled and then close the 
+        # library. This allows client to flush offset and process  any remianing messages 
